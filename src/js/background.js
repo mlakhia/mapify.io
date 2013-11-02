d0e7f32262a1b65a9fdae8591a524149a8d5159e
Michael Lakhia <mike@diputs.ca>
2013-11-01 21:16:50 -0400

    Merge cleanup

--- src/js/background.js
+++ src/js/background.js
@@ -10,7 +10,41 @@
 		--- http://leafletjs.com/examples/mobile.html
 	*/
 
-	/* // **BROWSER ACTION**
+	// **OMNIBOX SEARCH** 
+	chrome.omnibox.setDefaultSuggestion({"description" : "Search Kijiji for: %s"});
+
+	// This event is fired on all changes to the text in the omnibox in keyword (mk) mode
+	chrome.omnibox.onInputChanged.addListener(
+		function(text, suggest) {
+			//console.log('inputChanged: ' + text);
+
+			// KIJIJI SUGGESTIONS URL
+			//http://ac.classistatic.com/ac/201306010004/2/en_CA//t/h/e/20.js?callback=jsonp
+
+			/* // Only 5 will show at a time
+			suggest([
+			  {content: text + " 1", description: "1"},
+			  {content: text + " 2", description: "2"},
+			  {content: text + " 3", description: "3"},
+			  {content: text + " 4", description: "4"},
+			  {content: text + " 5", description: "5"}
+			]);*/
+	  });
+
+	chrome.omnibox.onInputEntered.addListener(
+		function(text) {
+			//var kijiji_search_url = "http://kijiji.ca/f-SearchAdRedirect?isSearchForm=true&Keyword="+text+"&CatId=0";
+			var kijiji_search_url = "http://kijiji.ca/f-"+text+"-Classifieds-W0QQKeywordZ"+text+"QQisSearchFormZtrue";
+			navigate(encodeURI(kijiji_search_url));
+	});
+
+	function navigate(url) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.update(tabs[0].id, {url: url});
+		});
+	}
+
+	/* // **BROWSER ACTION** (inactive)
 	// Listens for the Kijiji Mapify icon click - browser
 	chrome.browserAction.onClicked.addListener(browserActionCallback);
 	function browserActionCallback(tab) {
@@ -27,7 +61,7 @@
 	}
 	*/
 
-	// **PAGE ACTION**
+	// **PAGE ACTION** (active)
 	// Listens for the Kijiji Mapify icon click - page (in omnibar)
 	chrome.pageAction.onClicked.addListener(pageActionCallback);
 	function pageActionCallback(tab) {
@@ -43,92 +77,21 @@
         return;
 	}
 
-	// Listen for page action icon click
-	chrome.pageAction.onClicked.addListener(pageActionCallback);
-
-chrome.omnibox.setDefaultSuggestion({"description" : "Search Kijiji for: %s"});
-
-// This event is fired each time the user updates the text in the omnibox,
-// as long as the extension's keyword mode is still active.
-chrome.omnibox.onInputChanged.addListener(
-  function(text, suggest) {
-    
-    // KIJIJI SUGGESTIONS URL
-    //http://ac.classistatic.com/ac/201306010004/2/en_CA//t/h/e/20.js?callback=jsonp
-
-    console.log('inputChanged: ' + text);
-    /*suggest([
-      {content: text + " 1", description: "1"},
-      {content: text + " 2", description: "2"},
-      {content: text + " 3", description: "3"},
-      {content: text + " 4", description: "4"},
-      {content: text + " 5", description: "5"}
-    ]);*/
-  });
-
-// This event is fired with the user accepts the input in the omnibox.
-chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-
-  	//var kijiji_search_url = "http://kijiji.ca/f-SearchAdRedirect?isSearchForm=true&Keyword="+text+"&CatId=0";
-  	var kijiji_search_url = "http://kijiji.ca/f-"+text+"-Classifieds-W0QQKeywordZ"+text+"QQisSearchFormZtrue";
-
-  	navigate(encodeURI(kijiji_search_url));
-  
-  });
-
-function navigate(url) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: url});
-  });
-}
-
-/*
-
-http://toronto.kijiji.ca/f-SearchAdRedirect?isSearchForm=true&Location=1700272&Keyword=yayvalue&CatId=0	302	GET	toronto.kijiji.ca	/f-SearchAdRedirect?isSearchForm=true&Location=1700272&Keyword=yayvalue&CatId=0	 512 ms	2.18 KB	Complete	
-http://toronto.kijiji.ca/f-yayvalue-Classifieds-W0QQKeywordZyayvalueQQisSearchFormZtrue	200	GET	toronto.kijiji.ca	/f-yayvalue-Classifieds-W0QQKeywordZyayvalueQQisSearchFormZtrue	 450 ms	13.73 KB	Complete	
-
-	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	    console.info("This is the url of the tab = " + changeInfo.url);
-	    // do stuff with that url here....
-	});
-
-	
-	chrome.tabs.onUpdated.addListener(// Called when the url of a tab changes
-	function (tabId, changeInfo, tab) {
-		
-		console.log(tabId);
-		console.log(changeInfo);
-		console.log(tab);
-		console.log(changeInfo.url);
-
-		if (tab && tab.url && tab.url.indexOf('kijiji.ca') > -1) {
-			// Show page action icon in omnibar
-			chrome.pageAction.show(tabId);
-		}else{
-			// Hide
-			chrome.pageAction.hide(tabId);			
-		}
-	});
-*/
-
 	/* chrome.pageAction.setIcon
 	 * 
 	 * FIXME: Icon gets distorted even when using the higher res icon38
 	 * 			Using pageAction.show with default icon as it's better looking
 	 */
-
 	function validListingPage(tab){
-
-	    	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-		    	//console.log(tabs);
-		    	chrome.pageAction.show(tab.id);
-			});
-			/*
-	    	chrome.pageAction.setIcon({tabId: tab.id, path: "images/icon38.png"}, function(){
-				chrome.pageAction.show(tab.id);
-			});*/
-	    	console.log("MapifyKijiji: VALID Kijiji LISTING URL:", tab.url);
+    	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+	    	//console.log(tabs);
+	    	chrome.pageAction.show(tab.id);
+		});
+		/*
+    	chrome.pageAction.setIcon({tabId: tab.id, path: "images/icon38.png"}, function(){
+			chrome.pageAction.show(tab.id);
+		});*/
+    	console.log("MapifyKijiji: VALID Kijiji LISTING URL:", tab.url);
 	}
 
 	function checkForValidUrl(tab) {
@@ -139,12 +102,12 @@ http://toronto.kijiji.ca/f-yayvalue-Classifieds-W0QQKeywordZyayvalueQQisSearchFo
 	    	listing2 = /kijiji.ca\/p\//;
 
 	    if(listing1.test(tab.url)){
-	    	// LISTING PAGE
+	    	// LISTING PAGE: /f-
 	    	validListingPage(tab);
 	    	console.log("MapifyKijiji: VALID Kijiji LISTING URL (/f-):", tab.url);
 
 	    }else if(listing2.test(tab.url)){
-	    	// LISTING PAGE
+	    	// LISTING PAGE: /p/
 	    	validListingPage(tab);
 	    	console.log("MapifyKijiji: VALID Kijiji LISTING URL (/p/):", tab.url);
 
@@ -160,7 +123,6 @@ http://toronto.kijiji.ca/f-yayvalue-Classifieds-W0QQKeywordZyayvalueQQisSearchFo
 		    chrome.pageAction.hide(tab.id);
 	    	console.log("MapifyKijiji: INVALID URL:", tab.url);				
 		}
-		
 	}
 
 	// Listen for changes to the URL of any tab
