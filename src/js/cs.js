8d75dc1d859013f5ffd2aa3211f2848cc5bf8753
Michael Lakhia <mike@diputs.ca>
2013-11-04 00:24:43 -0500

    Improving map/scrapping code to be more modular

--- src/js/cs.js
+++ src/js/cs.js
@@ -46,6 +46,8 @@
 	// updates pagination variables, checks for current, next and set last page bool
 	// only run at the beginning, similar code in processNextPage
 	function updatePagination(){
+		csLog("updatePagination");
+
 		currentPage = $('.paginationBottomBg .currentPage').text().trim();
 		nextPage = $('.paginationBottomBg .currentPage').next().text().trim();
 		nextPageUrl = $(".paginationBottomBg .prevNextLink:contains('Next')").attr('href');
@@ -75,6 +77,8 @@
 
 	// ** NEXT PAGE PROCESSING **
 	function processNextPage(){
+		csLog("processNextPage");
+
 		if(nextPageUrl != "" && nextPageUrl != undefined){
 		var $tempHtml = $('<div>');
 			$tempHtml.load(nextPageUrl, function(response, status, xhr) {
@@ -131,82 +135,87 @@
 
 					runCleanup();
 
+					// unlock auto-load
 					setTimeout(function (){
 						processAllowed = true;
 					}, 300);
-					//csLog(listingRows);
-					
-					//listingRows.forEach(function(element, index, array){
-						//csLog("Appending: " + element);
-						//$('#SNB_Results tr:last').append(element);
-					//});
 				}
 			});
 		}
 	}
 
 	// ** LISTINGS **
-
-	var listingLinks = [];
-	var listingAddresses = [];
-	var listingCoords = [];
-	var newListingLinks = []; // used for infinite-scroll loaded listings
-
+	// Finds all the listing links on the page and triggers processing of those links processLinks()
 	function startPageInspection(){
+		csLog("startPageInspection");
 
+		var listingLinks = [];
 		$('a.adLinkSB').each(function(){
 			listingLinks.push($(this).attr('href'));
 		});
-		console.log(listingLinks);
+		csLog("=== startPageInspection : listingLinks : ===");
+		csLog(listingLinks);
 
 		// start processing
-		processLinks();
+		processLinks(listingLinks);
 	}
 
-	function processLinks(){
+	/* 
+	 * Takes an array of links, visits each and builds listingDetails 
+	 *  when all links have been visited, make call to processAddresses()
+	 */
+	function processLinks(links){
+		csLog("processLinks");
 
-		var listLength = listingLinks.length;
-		var processedLinksCount = 0;
-		listingLinks.forEach(function(element, index, array){
+		var listingDetails = [];
+		var listLength = links.length;
+
+		console.log(links);
+
+		for(var i = 0; i < listLength; i++){
+
+			console.log("Scrapping1: ",i);
+
+			var theIndex = i;
+			var theLink = links[i];
 
 			var $tempHtml = $('<div>');
-			$tempHtml.load(element + ' #attributeTable', function(response, status, xhr) {
+			$tempHtml.load(theLink + ' #attributeTable', function(response, status, xhr) {
 				
-				console.log("Scrapping",index);
+				console.log("Scrapping2: ",theIndex);
+				console.log("Scrapping2: ",theLink);
 
 				if (status == "error") {
 					var msg = "Sorry but there was an error: ";
 					console.log(msg + xhr.status + " " + xhr.statusText);
 				}else{
-
 					// listing page loaded
 
 					// grab the map anchor elements parent (tr)
 					var alterHtml = $('a.viewmap-link', this).parent();
 					// remove the anchor
 					$("a", alterHtml).remove();
-
 					// we now have just the address, save in array
-					listingAddresses.push(alterHtml.text());
+					listingDetails.push([theLink, alterHtml.text()]);
 					//console.log(alterHtml.text());
 				}
 
-				processedLinksCount++;
-
 				//console.log(index);
-				if(processedLinksCount == listLength){
-					processAddresses();
+				if(i == listLength){
+					csLog("i == listLength");
+					processAddresses(listingDetails);
 				}
-
 			});
-		});
+		}
 	}
 
-	function processAddresses(){
-		console.log(listingAddresses);
-
+	function processAddresses(listingLinksAddresses){
+		csLog("processAddresses");
+		//console.log(listingLinksAddresses);
+		
+		var listingCoords = [];
 		var processedAddressCount = 0;
-		for(var i=0; i<listingAddresses.length; i++){
+		for(var i=0; i<listingLinksAddresses.length; i++){
 
 			processedAddressCount++;
 
@@ -214,7 +223,10 @@
 
 			//ex: http://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true
 
-			var encoded_address = encodeURIComponent(listingAddresses[i]);
+			csLog("listingLinksAddresses"+listingLinksAddresses);
+			csLog("listingLinksAddresses[1][i]"+listingLinksAddresses[1][i]);
+
+			var encoded_address = encodeURIComponent(listingLinksAddresses[1][i]);
 			var compiled_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + encoded_address + "&sensor=false"
 
 			$.getJSON( compiled_url, {
@@ -236,7 +248,7 @@
 
 				if(processedAddressCount == listingAddresses.length){
 					console.log("geocoords length",listingAddresses.length);
-					initialize_map();
+					updateMap(listingCoords);
 				}
 
 				//console.log(data.results[0].formatted_address);
@@ -258,6 +270,7 @@
 
 	// ** PAGE CLEANUP **
 	function runCleanup(){
+		csLog("runCleanup");
 		// cleanup ads
 		$('#topAdSense, #bottomAdSense, .GoogleActiveViewClass, .sbTopadWrapper').remove();
 		$('#lmDIYads, #inlineBanner').parent().remove();
@@ -273,123 +286,143 @@
 	var lng = -73.5667;
 
 	function injectMap(){
+		csLog("injectMap");
 		$('tr.resultsTableSB:first').before('<tr><td colspan="100%"><div id="map-canvas" style="width:'+$('#SNB_Results').width()+'px;height:400px;"></div></td></tr>');
 	}
 
-	function initialize_map() {
-		csLog("initialize_map");
+	function initializeMap() {
+		csLog("initializeMap");
 		L.Icon.Default.imagePath = chrome.extension.getURL("images");
 
-
-
-		if(map)
+		if(map){
 			map.remove();
+		}
 
 		csLog("lat:"+lat);
 		csLog("lng:"+lng);
 		map = L.map('map-canvas').setView([lat, lng], 10);
 
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			maxZoom: 12,
+			center: [lat, lng],
+			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 		}).addTo(map);
+	}
 
-		listingCoords.forEach(function(element, index, array){
+	function updateMap(listOfMarkers){
+		csLog("updateMap");
+		for(var i = 0; i < listOfMarkers.length; i++){
 			var marker = L.marker( [ element[0], element[1] ] )
-							.bindPopup(element[2] + '<p><a href="'+listingLinks[index]+'">View Listing</a>')
+							.bindPopup(element[2] + '<p><a href="'+listingLinks[i]+'">View Listing</a>')
 							.addTo(map);
-		});
-
+		}
 	}
 
 	// ** INIT **
+	var processAllowed = true;
+	function initInfiniteScroll(){
+		csLog("initInfiniteScroll");
+
+	    $(window).scroll(function(){
+	        var wintop = $(window).scrollTop(), 
+	        	docheight = $(document).height(), 
+	        	winheight = $(window).height();
+	        var  scrolltrigger = 0.95;
+
+	        if  ((wintop/(docheight-winheight)) > scrolltrigger && 
+	        		processAllowed == true) {
+	        	
+	        	if(lastPage == false){
+	        		processAllowed = false;
+					processNextPage();
+				}
+	        	csLog("scrolling active");
+	        }
+	    });
+	}
+
+    function updateGeolocation(){
+		csLog("updateGeolocation");
+
+	    // Get Geolocation
+		if (navigator.geolocation) {
+			csLog('Geolocation is supported!');
+			navigator.geolocation.getCurrentPosition(
+				// success
+				function(position) {
+					lat = position.coords.latitude;
+					lng = position.coords.longitude;
+
+					if(map){
+						map.panTo([lat, lng], {
+							animate: true, // If true, panning will always be animated if possible. If false, it will not animate panning.
+							duration: 0.25, // Duration of animated panning.
+							easeLinearity: 0.25, // The curvature factor of panning animation easing (third parameter of the Cubic Bezier curve). 1.0 means linear animation, the less the more bowed the curve.
+							noMoveStart: false // If true, panning won't fire movestart event on start (used internally for panning inertia).
+						});
+					}
+
+					csLog("position.coords:"+position.coords);
+					csLog("position.coords.latitude:"+position.coords.latitude);
+					csLog("position.coords.longitude:"+position.coords.longitude);
+					csLog("position.coords.accuracy(meters):"+position.coords.accuracy); // in meters
+				},
+				// error
+				function(positionError) {
+					console.log(positionError);
+					csLog("Geolocation: Error occurred. Error code: " + positionError.code);
+					// error.code can be:
+					//   0: unknown error
+					//   1: permission denied
+					//   2: position unavailable (error response from locaton provider)
+					//   3: timed out
+				}
+			);
+		}else{
+			csLog('Geolocation is not supported for this Browser/OS version yet.');
+		}
+    }
 
 	runCleanup();
+	updateGeolocation();
+    injectMap();
+    initializeMap();
+	
 
-	var processAllowed = true;
-    $(window).scroll(function(){
-        var wintop = $(window).scrollTop(), 
-        	docheight = $(document).height(), 
-        	winheight = $(window).height();
-        var  scrolltrigger = 0.95;
-
-        if  ((wintop/(docheight-winheight)) > scrolltrigger &&
-        		processAllowed == true) {
-        	
-        	if(lastPage == false){
-        		processAllowed = false;
-				processNextPage();
-			}
-        	csLog("scrolling active");
-        }
-    });
-    //#sbResultsListing
-
-    // Get Geolocation
-	if (navigator.geolocation) {
-		csLog('Geolocation is supported!');
-		navigator.geolocation.getCurrentPosition(
-			// success
-			function(position) {
-				lat = position.coords.latitude;
-				lng = position.coords.longitude;
-
-				csLog("position.coords:"+position.coords);
-				csLog("position.coords.latitude:"+position.coords.latitude);
-				csLog("position.coords.longitude:"+position.coords.longitude);
-				csLog("position.coords.accuracy(meters):"+position.coords.accuracy); // in meters
-			},
-			// error
-			function(positionError) {
-				console.log(positionError);
-				csLog("Geolocation: Error occurred. Error code: " + positionError.code);
-				// error.code can be:
-				//   0: unknown error
-				//   1: permission denied
-				//   2: position unavailable (error response from locaton provider)
-				//   3: timed out
-			}
-		);
-	}else{
-		csLog('Geolocation is not supported for this Browser/OS version yet.');
+	/*
+	if($('#SNB_Results').length > 0){
+		csLog("Listing Table Found");
+	} else {
+		csLog("Listing Table NOT Found");
 	}
+	*/
 
 	window.onload = function(){
-
 		setTimeout(function (){
-             updatePagination();
-             injectMap();
-             startPageInspection();
-         }, 300);
-
-		chrome.extension.onMessage.addListener(function(message,sender,sendResponse){
-			csLog("Received message:" + message);
-			//csLog("Received from:" + sender);
-
-			if(message == "iconClicked"){
-				csLog("Known Message: iconClicked");
-
-				if($('#SNB_Results').length > 0){
-					csLog("Listing Table Found");
-					sendResponse({type:"success"});
-
-					// Go
-					//injectModal();
-					//buildModal();
-					injectMap();
-					startPageInspection();					
-					
-				} else {
-					csLog("Listing Table NOT Found");
-					sendResponse({type:"fail"});
-				}
-			}else{
-				csLog("Unknown Message: " + message);
-				sendResponse({type:"fail"});
-			}
-			
-		});		
+			updatePagination();
+			initInfiniteScroll();
+			startPageInspection();
+		}, 300);
 	}
+
+	/* 
+	 * Communicate with the Background Script
+	 *
+	 */
+	chrome.extension.onMessage.addListener(function(message,sender,sendResponse){
+		csLog("Received message:" + message);
+		//csLog("Received from:" + sender);
+
+		if(message == "iconClicked"){
+			csLog("Known Message: iconClicked");
+			
+			updateGeolocation();
+
+		}else{
+			csLog("Unknown Message: " + message);
+			sendResponse({type:"fail"});
+		}
+	});
 
 	csLog("(end of cs)");
 })()
